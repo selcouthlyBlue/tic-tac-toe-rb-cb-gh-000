30353d2a68d62d4d2d345ac838b7216b57563e0d
Jerome Patrick Gonzalvo <jvgonzalvo@up.edu.ph>
2018-06-07 09:01:14 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -11,6 +11,10 @@ def input_to_index(number_input)
   return number_input.to_i - 1
 end
 
+def move(board, index, char="X")
+  board[index] = char
+end
+
 def display_row(board, row_index)
   puts " #{board[3 * row_index]} | #{board[3 * row_index + 1]} | #{board[3 * row_index + 2]} "
 end
